3fa0fdf3cda1885ead597b5e7fc22d38d360d257
Niall McGuinness <niall.mcguinness@dkit.ie>
2026-02-25 14:46:31 +0000

    First commit

--- src/main/java/org/example/observer/Main.java
+++ src/main/java/org/example/observer/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
             Main theApp = new Main();
             theApp.run();
-            
         }
 
         public void run()
