1c73b977c9cb2f4201154ab67b56203a1a7050fd
Niall McGuinness <niall.mcguinness@dkit.ie>
2026-02-27 12:48:32 +0000

    Re-factored for CollectibleItem

--- src/main/java/org/example/observer/controller/EventDispatcher.java
+++ src/main/java/org/example/observer/controller/EventDispatcher.java
@@ -5,8 +5,29 @@ import org.example.observer.model.GameEvent;
 import java.util.ArrayList;
 import java.util.List;
 
+//how to make a Singleton class in Java?
+
 public class EventDispatcher {
+    //step 3 - make the reference to the SINGLE event dispatcher
+    private static EventDispatcher instance;
     private List<ISystem> eventHandlers = new ArrayList<>();     //FIFO queue
+
+    //step 1 - make private
+    private EventDispatcher() {
+
+    }
+
+    //step 2 - make a function that makes ONLY 1 instance and returns it
+    public static EventDispatcher getInstance(){
+
+        if(instance == null) {
+            //notice we now CAN internally call constructor
+            instance = new EventDispatcher();
+        }
+        return instance;
+    }
+
+
 
     public void register(ISystem system){
         eventHandlers.add(system);
